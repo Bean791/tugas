2b28d76b99a078ba158476d69b7e30b134da3820
Bean791 <bean90917@gmail.com>
2022-05-05 14:34:03 +0700

    finising tahap 1

--- sifat.js
+++ sifat.js
@@ -1,5 +1,16 @@
-const pen=document.querySelector('#Akhir');
-const a=document.querySelector(".tentang");
-pen.addEventListener('click',function(){
-a.classList.add('tampil');
+const bi=document.querySelector('#bukaisi');
+const b=document.querySelector("#isi");
+bi.addEventListener('click',function(){
+b.classList.toggle('hilang');
 })
+const pen=document.querySelector('#Akhir');
+const a=document.querySelector("#tentang");
+pen.addEventListener('click',function(){
+a.classList.toggle('hilang');
+})
+const ak=document.querySelector('#akhirku');
+const c=document.querySelector("#Akhir");
+ak.addEventListener('click',function(){
+c.classList.toggle('hilang');
+})
+
